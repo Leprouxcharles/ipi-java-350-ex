7c71dda46eae1a944af8108deb6cfd1fa4ef96d8
charles.leproux <Minhtam1510!>
2020-03-02 16:47:26 +0100

    add test

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -1,12 +1,23 @@
 package com.ipiecoles.java.java350.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class EmployeRepositoryTest {
+
+    @Autowired
+    EmployeRepository employeRepository;
 
     @Test
     public void TestFindLastMatricule(){
-
+        //Given
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+        //Then
+        Assertions.assertThat(lastMatricule).isNull();
     };
 
 }
