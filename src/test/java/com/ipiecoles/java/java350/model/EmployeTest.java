595e3c680a17763f9ef6a29aa0290ea57f203c62
charles.leproux <Minhtam1510!>
2020-03-02 15:58:47 +0100

    pom.xml add plugin

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -2,6 +2,8 @@ package com.ipiecoles.java.java350.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 
@@ -67,5 +69,29 @@ public class EmployeTest {
     }
 
 
+    @Test
+    public void testGetPrimeAnnuellePrimeAnciennete(){
+        //Given = Initialisation des données d'entrée
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(3));
+
+        //When = Exécution de la méthode à tester
+        Integer nombreAnneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then = Vérifications de ce qu'a fait la méthode
+        Assertions.assertThat(nombreAnneeAnciennete).isEqualTo(0);
+    }
+
+
+    @ParameterizedTest(name = "immat {0} est valide : {1}")
+    @CsvSource({
+            "'XXXXX', false",
+            "'AA-123-BB', true"
+    })
+    void testCheckBadImmatriculation(String immat, Boolean result) {
+        //Given, When, Then
+        Employe employe = new Employe();
+        Assertions.assertThat(employe.getPrimeAnnuelle()).isEqualTo(result);
+    }
 
 }
