0addf697c719055329fb6202052e73338d1f78dd
charles.leproux <Minhtam1510!>
2020-03-03 09:27:28 +0100

    add test repository EmployeRepositoryTest

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.ipiecoles.java.java350.repository;
 
+import com.ipiecoles.java.java350.model.Employe;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @SpringBootTest
 public class EmployeRepositoryTest {
@@ -18,6 +23,26 @@ public class EmployeRepositoryTest {
         String lastMatricule = employeRepository.findLastMatricule();
         //Then
         Assertions.assertThat(lastMatricule).isNull();
+    };
+
+    @Test
+    public void TestFindLastMatricule3Employes(){
+
+        //Given
+        Employe e1 = new Employe("Doe","John","T12345", LocalDate.now(),1500d,1,1.0);
+        Employe e2 = new Employe("Doe","Jane","C45678", LocalDate.now(),2500d,2,0.5);
+        Employe e3 = new Employe("Doe","Peter","M34567", LocalDate.now(),3500d,1,0.5);
+        employeRepository.saveAll(Arrays.asList(e1,e2,e3));
+
+        //When
+        String lastMatricule = employeRepository.findLastMatricule();
+        //Then
+        Assertions.assertThat(lastMatricule).isEqualTo("45678");
+    };
+
+    @BeforeEach
+    public void before(){
+        employeRepository.deleteAll();
     };
 
 }
